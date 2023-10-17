7d465b3907e88f6ee2674c191b5e854383445ae5
Sharanvthu <104510061+Sharanvthu@users.noreply.github.com>
2023-10-17 15:20:14 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,14 @@
+package srtestMavenByUday.Maven1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext context=new ClassPathXml
+    
+}
+}
---
+++ College.java
@@ -0,0 +1,21 @@
+package srtestMavenByUday.Maven1;
+
+public class College {
+	private String name;
+	private Student std;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Student getStd() {
+		return std;
+	}
+	public void setStd(Student std) {
+		this.std = std;
+	}
+	
+	
+
+}
